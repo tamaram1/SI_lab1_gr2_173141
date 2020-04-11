8b8078d0249446fa44e987f8268a29356410ba91
Tamara <markovskat7@gmail.com>
2020-04-11 23:12:38 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -3,10 +3,52 @@ class Student {
 	String firstName;
 	String lastName;
 
+	List<Integer> grades = new ArrayList<Integer>();
+
 	//TODO constructor
+	public Student(String index, String firstName, String lastName, List<Integer> list) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.list = list;
+	}
+
 
 	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
 
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+
+	
 	public double getAverage() {
 		//TODO
 	}
